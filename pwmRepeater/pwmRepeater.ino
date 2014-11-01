7b6ba7f1bfb5a785b2de1744e8b8a5e0d6af2c04
Harris Christiansen (hc15ret) <harrischristiansen@mac.com>
2014-10-31 23:22:40 -0400

    Started YHack - Ouija Board

--- pwmRepeater/pwmRepeater.ino
+++ pwmRepeater/pwmRepeater.ino
@@ -5,14 +5,22 @@ int pwmIN = 3;
 int pwmOut = 9;
 int resultIN = 10;
 
+int currentSpeed = 0;
+
 void setup() {
   pinMode(pwmIN, INPUT);
   pinMode(resultIN, INPUT);
   Serial.begin(57600);
+  Serial.setTimeout(1);
 }
 
 void loop() {
   //analogWrite(pwmOut, pulseIn(pwmIN, HIGH)/7);
-  Serial.println(pulseIn(resultIN, HIGH));
+  //Serial.println(pulseIn(resultIN, HIGH));
+  if(Serial.available() > 0) {
+    currentSpeed = Serial.parseInt();
+    Serial.println(currentSpeed);
+  }
+  analogWrite(pwmOut, currentSpeed);
   delay(100);
 }
