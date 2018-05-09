eb289a06cabb316bb62704596ba366e03c268c4a
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2018-05-09 00:19:21 -0400

    Routine for white globe strip at Naman's

--- led_whiteGlobe/led_whiteGlobe.ino
+++ led_whiteGlobe/led_whiteGlobe.ino
@@ -3,60 +3,81 @@
 
 #include <Adafruit_NeoPixel.h>
 
-#define NUM_LEDS 20
+#define NUM_LEDS 19
+#define NUM_LEDS_TOTAL 20
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, 6, NEO_RGB + NEO_KHZ400);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS_TOTAL, 6, NEO_RGB + NEO_KHZ400);
 
 void setup() {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 }
 
-int start=0;
+int start = 0;
+int totalcount = 0;
 
-int red=255;
-int green=0;
-int blue=0;
+int red = 255;
+int green = 0;
+int blue = 0;
 
+int color = 0;
+int currentDelay = 10;
 void loop() {
-  int color = 0;
-  
-  // Rainbow Wheel
-  for(int x=0; x<NUM_LEDS; x++) {
-    color = (x+start) % NUM_LEDS;
-    color = (int) color/5;
-    if(color==0) {
-      strip.setPixelColor(x, strip.Color(10, 0, 0));
-    } else if(color==1) {
-      strip.setPixelColor(x, strip.Color(0, 10, 0));
-    } else {
-      strip.setPixelColor(x, strip.Color(0, 0, 10));
+  totalcount++;
+
+  if (totalcount < 100) { // Blackness With Flashes
+    currentDelay = 100;
+    for (int x=0; x<NUM_LEDS; x++) {
+      if (totalcount*7 % NUM_LEDS == x) {
+        strip.setPixelColor(x, strip.Color(200, 200, 200));
+      } else {
+        strip.setPixelColor(x, strip.Color(0, 0, 0));
+      }
     }
-    
+  } else if (totalcount < 200) { // Fade In
+    currentDelay = 100;
+    for (int x=0; x<NUM_LEDS; x++) {
+      int brightness = (totalcount-100)*255/100;
+      strip.setPixelColor(x, strip.Color(brightness, brightness, brightness));
+    }
+  } else if (totalcount < 4000) { // Color Fader
+    currentDelay = 10;
+    if ((red!=0 && blue!=0) || blue==255) {
+      blue--;
+      red++;
+    }
+    if ((red!=0 && green!=0) || red==255) {
+      red--;
+      green++;
+    }
+    if ((green!=0 && blue!=0) || green==255) {
+      green--;
+      blue++;
+    }
+    for (int x=0; x<NUM_LEDS; x++) {
+      strip.setPixelColor(x, strip.Color(red+x, green, blue));
+    }
+  } else { // Rainbow Wheel
+    currentDelay = 100;
+    for (int x=0; x<NUM_LEDS; x++) {
+      color = (x+start) % NUM_LEDS;
+      color = (int) color/5;
+      if (color==0) {
+        strip.setPixelColor(x, strip.Color(255, 0, 0));
+      } else if (color==1) {
+        strip.setPixelColor(x, strip.Color(0, 255, 0));
+      } else {
+        strip.setPixelColor(x, strip.Color(0, 0, 255));
+      } 
+    }
+    start++; // Step To Next Step
+    if (start == NUM_LEDS) { start=0; }
   }
-  start++; // Step To Next Step
-  if(start == NUM_LEDS) { start=0; }
-  
-  
-  /*
-  // Fader
-  if((red!=0&&blue!=0)||blue==255) {
-    blue--;
-    red++;
+
+  if (totalcount > 10000) {
+    totalcount = 0;
   }
-  if((red!=0&&green!=0)||red==255) {
-    red--;
-    green++;
-  }
-  if((green!=0&&blue!=0)||green==255) {
-    green--;
-    blue++;
-  }
-  for(int x=0;x < NUM_LEDS;x++) {
-    strip.setPixelColor(x, strip.Color(red+x, green, blue));
-  }
-  */
   
   strip.show();
-  delay(1000); // 100=chaser, 10=fader
+  delay(currentDelay); // 100=chaser, 10=fader
 }
